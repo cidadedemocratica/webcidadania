9e495388423bebc8c1451853a1076addc5e79406
Marcos Lopes <marcosmlopes01@gmail.com>
2012-06-20 20:08:10 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webcidadania::Application.config.secret_token = 'fb2c1fdd3ba35cf833e9a3f5248234d85192cbe9a43a379b5549b616b4a373df2e8d20fa1bc1c6d99a1212b495129e558c12df19e2e00264e17bc256981138d3'
